7664d620b5beff3a113d83143e1cbfbf2a58b833
sahrul mahani <sahrul.mahani@gmail.com>
2023-05-19 09:58:47 +0800

    second commits

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,23 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function print()
+    {
+        $format = [
+            'format'    => 'A4',
+            'margin_left' => 20,
+            'margin_right' => 20,
+            'margin_top' => 30,
+            'margin_bottom' => 10,
+            'margin_header' => 10,
+            'margin_footer' => 9,
+        ];
+        $mpdf = new \Mpdf\Mpdf($format);
+        $mpdf->SetTitle('judu l oke');
+        // $html = view('pdf/main');
+        $mpdf->WriteHTML('<h1>okok</h1>');
+
+        return redirect()->to($mpdf->Output());
+    }
 }

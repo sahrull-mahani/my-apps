edf42109bf4f897a02179f7bc88dd9caabc7a0a1
sahrul mahani <sahrul.mahani@gmail.com>
2023-05-20 22:10:01 +0800

    membuat design spt THL

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -12,18 +12,31 @@ class Home extends BaseController
     public function print()
     {
         $format = [
-            'format'    => 'A4',
-            'margin_left' => 20,
-            'margin_right' => 20,
-            'margin_top' => 30,
+            'format'    => 'Legal',
+            'default_font' => 'tahoma',
+            'default_font_size' => 12,
+            'margin_left' => 10,
+            'margin_right' => 10,
+            'margin_top' => 45,
             'margin_bottom' => 10,
             'margin_header' => 10,
             'margin_footer' => 9,
         ];
         $mpdf = new \Mpdf\Mpdf($format);
-        $mpdf->SetTitle('judu l oke');
-        // $html = view('pdf/main');
-        $mpdf->WriteHTML('<h1>okok</h1>');
+        $mpdf->SetFont('tahoma');
+        $pdfstyle = file_get_contents(__DIR__ . '/../../public/assets/css/pdf.css');
+        $mpdf->WriteHTML($pdfstyle, \Mpdf\HTMLParserMode::HEADER_CSS);
+
+        $mpdf->SetTitle('SPT');
+        $mpdf->SetSubject('Report');
+        $mpdf->SetAuthor('MSM');
+
+        $header = view('App\Views\pdf\header');
+        $mpdf->defaultheaderline = 0;
+        $mpdf->SetHeader($header);
+
+        $html = view('pdf/main');
+        $mpdf->WriteHTML($html);
 
         return redirect()->to($mpdf->Output());
     }
